f7334f3d4254be90a50f7fce0337da4bcf14347c
Iyanda Dotun <olamidotun225@gmail.com>
2026-08-25 00:35:33 +0100

    chore(auth): update package configuration and public barrel exports

--- packages/features/authentication/src/index.ts
+++ packages/features/authentication/src/index.ts
@@ -1,1 +1,8 @@
-export * from './lib/authentication.js';
+export * from './application/use-auth-flow.js';
+export * from './application/use-session-manager.js';
+export * from './application/use-auth-store.js';
+
+// Re-export specific queries and mutations if external features need them
+export * from './data/queries/index.js';
+export * from './data/mutations/index.js';
+export * from './domain/entities/auth-account.entity.js';
--- packages/features/authentication/src/lib/authentication.spec.ts
+++
@@ -1,7 +0,0 @@
-import { authentication } from './authentication.js';
-
-describe('authentication', () => {
-  it('should work', () => {
-    expect(authentication()).toEqual('authentication');
-  });
-});
