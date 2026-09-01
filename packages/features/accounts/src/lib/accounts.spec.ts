92f466ebc4b48f01c915feb675c4b9f425d57398
Iyanda Dotun <olamidotun225@gmail.com>
2026-09-01 16:35:27 +0100

    refactor(accounts): remove unused files

--- packages/features/accounts/src/lib/accounts.spec.ts
+++
@@ -1,7 +0,0 @@
-import { accounts } from './accounts.js';
-
-describe('accounts', () => {
-  it('should work', () => {
-    expect(accounts()).toEqual('accounts');
-  });
-});
--- packages/features/accounts/src/lib/accounts.ts
+++
@@ -1,3 +0,0 @@
-export function accounts(): string {
-  return 'accounts';
-}
